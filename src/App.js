bbe2e70e6325fb03e37cf458169a2735406d6b07
yiyanglin0102 <yiyanglin0102@gmail.com>
2023-03-09 23:17:42 -0500

    Setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
-import { CartButtons } from './components'
-import { Testing } from './Testing';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import { Home, SingleProduct, Cart, Checkout, Error, About, Products, PrivateRoute } from './pages'
 
 function App() {
   return (
-    <div className="App">
-      <Testing />
-      <CartButtons />
-    </div>
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/products" element={<Products />} />
+
+        <Route path="/products" element={<SingleProduct />}>
+          <Route path=":id" element={<Products />} />
+        </Route>
+
+        <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="*" element={<Error />} />
+
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
